815e1844d5117c8dcf74a6f84b18437a448c44c4
2011B4A7584P <ashish.dalal.bitspilani@gmail.com>
2023-03-16 01:48:28 +0530

    added solution to game play analysis ii leetcode category easy->hard problem for review

---
+++ easy_hard/game_play_analysis_ii.sql
@@ -0,0 +1,19 @@
+WITH
+
+login_rankings (player_id, device_id,login_rank) AS
+
+(
+SELECT
+    a.player_id,
+    a.device_id,
+    DENSE_RANK() OVER(PARTITION BY a.player_id ORDER BY a.event_date) AS login_rank 
+FROM
+    activity a
+) 
+SELECT 
+	player_id, 
+	device_id 
+FROM 
+	login_rankings d
+WHERE 
+	d.login_rank = 1;
