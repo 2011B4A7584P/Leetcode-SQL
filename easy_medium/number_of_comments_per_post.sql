c9d79879f83710733d403d6582dd5431274b07f0
2011B4A7584P <ashish.dalal.bitspilani@gmail.com>
2023-03-21 21:26:09 +0530

    added solution to number_of_comments_per_post leetcode category easy->medium problem for review

---
+++ easy_medium/number_of_comments_per_post.sql
@@ -0,0 +1,13 @@
+SELECT
+    s1.sub_id AS post_id,
+    COUNT(DISTINCT s2.sub_id) AS number_of_comments
+FROM
+    submissions s1
+LEFT JOIN
+    submissions s2
+ON 
+    s1.sub_id = s2.parent_id
+WHERE
+    s1.parent_id IS NULL
+GROUP BY 
+    s1.sub_id;
