97e76043ef8fa48ffd7dc8f2cf9811388b4be2f8
2011B4A7584P <ashish.dalal.bitspilani@gmail.com>
2023-03-25 00:35:38 +0530

    added solution to top_travellers leetcode category easy_medium problem for review

---
+++ easy_medium/top_travellers.sql
@@ -0,0 +1,25 @@
+WITH 
+
+user_id_distance_info (name, id, travelled_distance) AS
+(
+SELECT
+    u.name,
+    u.id,
+    SUM(IFNULL(r.distance,0)) AS travelled_distance
+FROM
+    users u
+LEFT JOIN
+    rides r
+ON
+    u.id = r.user_id
+GROUP BY 
+    u.name,
+    u.id
+)
+SELECT
+    name,
+    travelled_distance
+FROM
+    user_id_distance_info
+ORDER BY 
+travelled_distance DESC, name ASC;    
