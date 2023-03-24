f9d31d130f2c4016dc4306310c13dd81068a3013
2011B4A7584P <ashish.dalal.bitspilani@gmail.com>
2023-03-24 22:32:52 +0530

    added solution to list_the_products_ordered_in_a_period leetcode category easy problem

---
+++ easy/list_the_products_ordered_in_a_period.sql
@@ -0,0 +1,16 @@
+SELECT
+    p.product_name,
+    SUM(o.unit) AS `unit` 
+FROM
+    products p
+LEFT JOIN
+    orders o
+ON 
+    p.product_id = o.product_id
+WHERE
+    MONTH(order_date) = 2
+AND YEAR(order_date) = 2020
+GROUP BY 
+    p.product_name
+HAVING 
+    SUM(o.unit) >= 100;
