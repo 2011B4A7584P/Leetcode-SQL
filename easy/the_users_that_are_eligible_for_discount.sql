d01378f49522e7b7adb010a2a1da5b68822ce743
2011B4A7584P <ashish.dalal.bitspilani@gmail.com>
2023-05-05 17:55:27 +0530

    updated solution to the_users_that_are_eligible_for_discount leetcode category easy problem

--- easy/the_users_that_are_eligible_for_discount.sql
+++ easy/the_users_that_are_eligible_for_discount.sql
@@ -2,11 +2,12 @@ CREATE PROCEDURE getUserIDs(startDate DATE, endDate DATE, minAmount INT)
 BEGIN
 	# Write your MySQL query statement below.
 	SELECT 
-        p.user_id
+        DISTINCT p.user_id
     FROM
         purchases p
     WHERE 
         p.time_stamp >= startDate 
     AND p.time_stamp <= endDate
-    AND p.amount >= minAmount;
+    AND p.amount >= minAmount
+    ORDER BY p.user_id;
 END
