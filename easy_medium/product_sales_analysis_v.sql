eb5bf9db95e9704e87e79ccd013e7a8ecce31d75
2011B4A7584P <ashish.dalal.bitspilani@gmail.com>
2023-05-05 17:15:05 +0530

    added solution to product_sales_analysis_v leetcode category easy->medium problem for review

---
+++ easy_medium/product_sales_analysis_v.sql
@@ -0,0 +1,11 @@
+SELECT
+    s.user_id,
+    SUM(s.quantity*p.price) AS spending
+FROM
+    sales s
+JOIN
+    product p
+ON 
+    s.product_id = p.product_id
+GROUP BY s.user_id
+ORDER BY spending DESC;
