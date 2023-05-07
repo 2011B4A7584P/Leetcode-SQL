246fb0b6b1405e422e9668d4e909aedab05d0667
2011B4A7584P <ashish.dalal.bitspilani@gmail.com>
2023-05-08 01:31:46 +0530

    added solution to warehouse_manager leetcode category easy->medium problem for review

---
+++ easy_medium/warehouse_manager.sql
@@ -0,0 +1,11 @@
+SELECT
+    w.name AS warehouse_name,
+    SUM(p.width * p.length * p.height*w.units) AS volume
+FROM
+    warehouse w
+JOIN 
+    products p
+ON
+    w.product_id = p.product_id
+GROUP BY 
+    w.name;
